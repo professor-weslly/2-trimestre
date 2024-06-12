1775e101621237c3c58285a01a6804ff024472a9
sunbooh <148074723+sunbooh@users.noreply.github.com>
2024-06-12 07:47:59 -0300

    ascds

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+cont caixapirncipal = document.querySelector(".caixa-principal");
+cont caixapirncipal = document.querySelector(".caixa-perguntas");
+cont caixapirncipal = document.querySelector(".caixa-alternativas");
+cont caixapirncipal = document.querySelector(".caixa-resultado");
+cont caixapirncipal = document.querySelector(".caixa-resultado");
 
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas;[
+            texto: "alternativa 1"
+            texto:"alternativa 2",
+        ]
+}
+    {
+        enunciado: "pergunta 2",
+        alternativas;[
+            texto: "alternativa 1"
+            texto: "alternativa 2"
+]
+}
+    {        enunciado: "pergunta 3",
+            alternativos;[
+                texto: "alternativa 1",
+                texto: "alternativa 2",
+            ]
+}
+    {
+        enunciado: "pergunta 4"
+        alternativas;[
+            texto: "alternativa 1"
+            texto: "alternativa 2"
+        ]
+ }
+    {
+        enunciado: "pergunta 5"
+        alternativas;[
+            texto: "alternativa 1"
+            texto: "alternativa 2"
+
+        ]
+}
+]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostrapergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas. textContent= perguntaAtual.enunciado;
+    mostraAlternativa();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+    const botaoAlternativas = document.createElement("button");
+    botãoAlternatiivas.textContent = alternativa;
+    caixaAlternativa.appendchild(botãoAlternativas);
+    }
+}
+
+mostraPerguntas ();
